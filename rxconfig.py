98d9c7223f357a559165047745e2a80638cd8f5f
Dave Amato <daveamato@live.com>
2025-11-13 11:06:31 -0500

    Update API URL to use configurable port

--- rxconfig.py
+++ rxconfig.py
@@ -6,12 +6,12 @@ import socket
 ipaddr = socket.gethostbyname(socket.gethostname())
 proxy_content = os.environ.get("PROXY_CONTENT", "TRUE").upper() == "TRUE"
 socks5 = os.environ.get("SOCKS5", "")
-socks5 = os.environ.get("API_URL", "http://")
+port = os.environ.get("PORT", "8000")
 
 print(f"PROXY_CONTENT: {proxy_content}\nSOCKS5: {socks5}")
 
 config = rx.Config(
-    api_url=f"http://{ipaddr}:8000",
+    api_url=f"http://{ipaddr}:{port}",
     app_name="StepDaddyLiveHD",
     proxy_content=proxy_content,
     socks5=socks5,
