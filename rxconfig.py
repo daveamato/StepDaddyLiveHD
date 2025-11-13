fc71489ceed07f622fe9eb45e77732f7460b1c24
Dave Amato <daveamato@live.com>
2025-11-13 11:04:54 -0500

    Add socket import and API URL handling

--- rxconfig.py
+++ rxconfig.py
@@ -1,13 +1,17 @@
 import reflex as rx
 import os
+import socket
 
 
+ipaddr = socket.gethostbyname(socket.gethostname())
 proxy_content = os.environ.get("PROXY_CONTENT", "TRUE").upper() == "TRUE"
 socks5 = os.environ.get("SOCKS5", "")
+socks5 = os.environ.get("API_URL", "http://")
 
 print(f"PROXY_CONTENT: {proxy_content}\nSOCKS5: {socks5}")
 
 config = rx.Config(
+    api_url=f"http://{ipaddr}:8000",
     app_name="StepDaddyLiveHD",
     proxy_content=proxy_content,
     socks5=socks5,
